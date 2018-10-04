7bdf729a48bfdfd30da6ad7b98911c642f4230d5
student <Sebsatianq1290@gmail.com>
2018-10-04 01:40:56 -0600

    minor name change

--- file-transfer-lab/framedSock.py
+++ file-transfer-lab/framedSock.py
@@ -44,26 +44,30 @@ def sendFile(sock, payload, debug=0):
           print("fileSend: sending %d byte file" % len(payload))
           print("the file name typed in is: ",payload)
           print("finding the path")
+     try:
+          writePayload = open(payload, "rb")
 
-     writePayload = open(payload, "rb")
-
-     #debugging
-     #print("and that file path is: ", rFilePath)
-     #print("reading file!")
+          #debugging
+          #print("and that file path is: ", rFilePath)
+          #print("reading file!")
      
-     length = writePayload.read(100)
-     if (length == 0):
-          print("error, empty or missing file!")
-     while (length):
-          sock.send(length)
           length = writePayload.read(100)
-     print("file has finished being sent")
-     sock.close()
-     writePayload.close()
+          print("length is", length)
+          empty = os.stat(payload).st_size == 0
+          if (empty):
+               print("error, empty file!")
+          while (length and not (empty)):
+               sock.send(length)
+               length = writePayload.read(100)
+          #print("file has finished being sent")
+          sock.close()
+          writePayload.close()
+     except IOError:
+          print ("File not found, please check your input")
 
 def getFile(sock, debug=0):
 
-  readF = open("rFile.txt", "wb")
+  readF = open("CopyFile.txt", "wb")
   length = sock.recv(100)
   while(length):
        readF.write(length)
